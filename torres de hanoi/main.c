06874d671820cab2f07670f942956e81545595b1
Israel Mendoza Rodriguez <ray200021@hotmail.com>
2019-12-03 20:39:30 -0600

    hanoi

---
+++ torres de hanoi/main.c
@@ -0,0 +1,12 @@
+#include "hanoi.h"
+int main(void) {
+
+
+   char torre1 = 'a', torre2 = 'b', torre3 = 'c';
+       int n;
+       printf ("ingres el numero de discos: ");
+       scanf("%d",&n);
+       jugada(n,torre1, torre2, torre3);
+      
+    return 0;
+}
